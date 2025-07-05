3c3ebc1c645e0a60769dcf522a7aedd1655dc2aa
Andres <andresrojas0248@gmail.com>
2025-07-04 22:12:49 -0500

    added project managewise-microservice

---
+++ src/main/java/com/horizon/managewise/managewisemicroservices/ManageWiseMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.horizon.managewise.managewisemicroservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageWiseMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManageWiseMicroserviceApplication.class, args);
+	}
+
+}
